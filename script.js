e9de7d42a887916dcea48d2e9ec179d8d3bf7486
AbudeyeW <AbudeyeW@gmail.com>
2020-12-13 16:29:26 -0600

    Ensures all values are received upon pressing Submit

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 // Write your JavaScript code here!
-//NEW CODE
+
+//Code for verification below
+window.addEventListener("load",function(){
+   let form = document.querySelector("form");
+   //Code for verifying all values are received
+   form.addEventListener("submit",function(event){
+      let pilotName = document.querySelector("input[name=pilotName]");
+      let copilotName = document.querySelector("input[name=copilotName]");
+      let fuelLevel = document.querySelector("input[name=fuelLevel]");
+      let cargoMass = document.querySelector("input[name=cargoMass]");
+      if(pilotName.value === ""|| copilotName.value===""|| fuelLevel ==="" || cargoMass ===""){
+         alert("Hola! Remember all fields are required");
+         event.preventDefault();
+      }
+      if(typeof pilotName.value !== "string" || typeof copilotName.value !== "string"){
+         alert("Hi! Please enter valid inputs. Make sure "+pilotName.value +" and "+copilotName.value+ " are both strings.");
+         event.preventDefault();
+      }
+      if(typeof fuelLevel.value !== "number" || typeof cargoMass.value !== "number"){
+         alert("Hi! Please enter valid inputs. Make sure "+fuelLevel.value +" and "+cargoMass.value+ " are both numbers.");
+         event.preventDefault();
+      }
+   })
+   alert("Hi!"+pilotName);
+})
+
+
+
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
